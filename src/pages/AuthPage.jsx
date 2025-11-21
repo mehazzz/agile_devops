47253889ccffa1fae9811e40a021af32d8509423
mehazzz <153672515+mehazzz@users.noreply.github.com>
2025-11-21 16:57:09 +0530

    Authpage fix
    Removed sensitive Firebase configuration details.

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -7,13 +7,7 @@ import { Book, Mail, Lock } from "lucide-react";
 import { FcGoogle } from "react-icons/fc";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCls2rFxMluWLSrw9cBTH97qiNGP778wDo",
-  authDomain: "edusync-92749.firebaseapp.com",
-  projectId: "edusync-92749",
-  storageBucket: "edusync-92749.firebasestorage.app",
-  messagingSenderId: "54356648048",
-  appId: "1:54356648048:web:1bb928bbf8e935c29554b5",
-  measurementId: "G-29PYP9TK1B"
+ //replace with your firebase api details 
 };
 
 const app = initializeApp(firebaseConfig);
